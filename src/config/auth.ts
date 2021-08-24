2e9e45227fd3eb38c48e5b9ba19efe821a55972b
jhongomes <jhonatangomes859@gmail.com>
2021-08-24 16:22:06 -0400

    update repository

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "bdf6660e03046c8aaf84ba38cc72f1a3",
+    expires_in_token: "15m",
+    secret_refresh_token: "f448149e381f59056b1a941b9d878c9c",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+}
